f6fb4298af29ef134222b59cc52d3649ae8174ff
Nicholas Jordaan <njordaan99@gmail.com>
2018-04-02 01:35:51 +0200

    Updated help/about dialog

--- SeriesTracker/SeriesTracker/Dialogs/AboutDialog.xaml.cs
+++ SeriesTracker/SeriesTracker/Dialogs/AboutDialog.xaml.cs
@@ -6,11 +6,11 @@ namespace SeriesTracker.Dialogs
 	public partial class AboutDialog : UserControl
 	{
 		#region Variables
-		string ProductName { get; }
-		string Version { get; }
-		string Copyright { get; }
-		string CompanyName { get; }
-		string Description { get; }
+		public string ProductName { get; set; }
+		public string Version { get; set; }
+		public string Copyright { get; set; }
+		public string CompanyName { get; set; }
+		public string Description { get; set; }
 		#endregion
 
 		public AboutDialog()
