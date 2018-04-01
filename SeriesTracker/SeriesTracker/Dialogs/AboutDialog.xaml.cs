9f686888d00fee3b2c4061e9f83b854bb19840c3
Nicholas Jordaan <njordaan99@gmail.com>
2018-04-01 22:53:05 +0200

    Added AboutDialog and removed formAbout

---
+++ SeriesTracker/SeriesTracker/Dialogs/AboutDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using SeriesTracker.Core;
+using System.Windows.Controls;
+
+namespace SeriesTracker.Dialogs
+{
+	public partial class AboutDialog : UserControl
+	{
+		#region Variables
+		string ProductName { get; }
+		string Version { get; }
+		string Copyright { get; }
+		string CompanyName { get; }
+		string Description { get; }
+		#endregion
+
+		public AboutDialog()
+		{
+			InitializeComponent();
+
+			ProductName = AppGlobal.AssemblyProduct;
+			Version = $"v{AppGlobal.AssemblyVersion}";
+			Copyright = AppGlobal.AssemblyCopyright;
+			CompanyName = AppGlobal.AssemblyCompany;
+			Description = AppGlobal.AssemblyDescription;
+
+			DataContext = this;
+		}
+	}
+}
