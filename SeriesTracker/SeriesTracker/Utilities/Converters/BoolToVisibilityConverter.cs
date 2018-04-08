e43847e9e7e4ab975682a57513123828db4e72ee
Nicholas Jordaan <njordaan99@gmail.com>
2018-04-08 21:49:09 +0200

    Added Converter and updated Overrides

--- SeriesTracker/SeriesTracker/Utilities/Converters/BoolToVisibilityConverter.cs
+++ SeriesTracker/SeriesTracker/Utilities/Converters/BoolToVisibilityConverter.cs
@@ -5,16 +5,16 @@ using System.Windows.Data;
 namespace SeriesTracker.Utilities.Converters
 {
 	[ValueConversion(typeof(Visibility), typeof(bool))]
-	public class VisibilityToBoolConverter : IValueConverter
+	public class BoolToVisibilityConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
 		{
-			return (Visibility)value == Visibility.Visible;
+			return (bool)value ? Visibility.Visible : Visibility.Hidden;
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
 		{
-			return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+			return (Visibility)value == Visibility.Visible;
 		}
 	}
 }
